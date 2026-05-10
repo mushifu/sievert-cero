7f3e60351a149fa2e00974a0623eae18da9fd231
almec <almetrash@gmail.com>
2026-05-10 20:58:32 +0200

    Supabase credentials + vercel redirects

--- sievert-cero/src/services/supabase.js
+++ sievert-cero/src/services/supabase.js
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://obvcomzemdirhbjrwzgw.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9idmNvbXplbWRpcmhianJ3emd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc4OTA2OTAsImV4cCI6MjA4MzQ2NjY5MH0.5sASBpkVRHHMS_hlnDSe6RBzlmDCiFPrMFi7tfBSFdI'
+const supabaseUrl =
+  import.meta.env.VITE_SUPABASE_URL
+
+const supabaseKey =
+  import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
